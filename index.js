665ca8e5ea1c3d8685cd377c969345cdd3eea940
Nick Cipollo <njc1115@gmail.com>
2016-09-17 14:29:57 -0400

    Added includes methods to RxMessage.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var Rx = require('rxjs/Rx');
 let slack = new RxSlack(token);
 
 slack.onMessage
-    .filter(message => message.isMention)
     .subscribe(message => console.log(message.toString()) );
 
 
