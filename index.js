cf2c1db2e0ce69c575f90ec6952f88beb57053a3
Nick Cipollo <njc1115@gmail.com>
2016-10-26 20:30:11 -0400

    Removed debugging code from index.js. Now exporting RxSlack.

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-let RxSlack = require('./lib/rxslack.js').RxSlack;
-let fs = require('fs');
-const token = process.env.RECV_TOKEN;
-var Rx = require('rxjs/Rx');
-
-let slack = new RxSlack(token);
-
-slack.onMessage
-    .subscribe(message => console.log(message.toString()) );
-
-slack.start();
-
+module.exports = {
+    RxSlack: require('./lib/rxslack.js').RxSlack
+};
